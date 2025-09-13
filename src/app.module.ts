cf59c01628a85e18079bbbe6334bbd5f36b75c47
Marcos Volpi <mark.landia@gmail.com>
2025-09-13 16:44:09 -0300

    Add user entity and module

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,9 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { PhotoModule } from './photo/photo.module';
+import { UserModule } from './user/user.module';
 @Module({
-  imports: [PhotoModule],
+  imports: [UserModule],
   controllers: [AppController],
   providers: [AppService],
 })
