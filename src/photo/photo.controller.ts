f77594e32f3c906fceabf91094b5d80aea3ed9b7
Marcos Volpi <mark.landia@gmail.com>
2025-09-13 16:39:08 -0300

    complete it

--- src/photo/photo.controller.ts
+++ src/photo/photo.controller.ts
@@ -1,12 +1,13 @@
 import { Controller, Get } from '@nestjs/common';
 import { PhotoService } from './photo.service';
+import { Photo } from './photo.entity';
 
 @Controller('photo')
 export class PhotoController {
   constructor(private readonly photoService: PhotoService) {}
 
   @Get('/')
-  photo(): string {
-    return 'testing';
+  photo(): Promise<Photo[]> {
+    return this.photoService.findAll();
   }
 }
