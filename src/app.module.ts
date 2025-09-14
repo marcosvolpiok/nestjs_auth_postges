fb0185879d657a1de80d1dee4335cd39e9080c44
Marcos Volpi <mark.landia@gmail.com>
2025-09-14 12:56:16 -0300

    Remove default files

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,7 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
 import { UserModule } from './user/user.module';
 import { ContactModule } from './contact/contact.module';
 @Module({
   imports: [UserModule, ContactModule],
-  controllers: [AppController],
-  providers: [AppService],
 })
 export class AppModule {}
